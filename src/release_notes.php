0b6007b838b264f2c43726b0dec76b27c73f4796
Willem van der Plaat <wplaat@gmail.com>
2016-01-25 19:31:07 +0100

    Update release notes

--- src/release_notes.php
+++ src/release_notes.php
@@ -33,6 +33,7 @@ $note[6] = '<div class="subparagraph">Version 0.6 (23-01-2016)</div>
 <li>General: Added database backup feature to setting page.</li>
 <li>General: Added minimun and maximum day value to weather information pages.</li>
 <li>General: Added source code documentation</li>
+<li>General: Added PlaatSoft Logo to about page</li>
 <li>Bugfix: Used energy years report show now correct Y axes scale.</li>
 <li>Bugfix: Selecting day in the future is now not possible anymore!</li>
 </ul>
