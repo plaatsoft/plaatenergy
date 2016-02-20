13461ee4447f170d5a9f4237c724a2ddc180840b
Willem van der Plaat <wplaat@gmail.com>
2016-02-20 07:23:07 +0100

    Add combobox e

--- src/database/patch-0.7.sql
+++ src/database/patch-0.7.sql
@@ -19,8 +19,14 @@ UPDATE config SET value="0.7" WHERE token='database_version';
 INSERT INTO config (`id`, `token`, `value`, `date`, readonly) VALUES (NULL, 'slide_show_on', 'false', SYSDATE(), 0);
 INSERT INTO config (`id`, `token`, `value`, `date`, readonly) VALUES (NULL, 'slide_show_page_delay', '10', SYSDATE(), 0);
 INSERT INTO config (`id`, `token`, `value`, `date`, readonly) VALUES (NULL, 'chart_legend', 'none', SYSDATE(), 0);
-INSERT INTO config (`id`, `token`, `value`, `date`, readonly) VALUES (NULL, 'chart_style', 'width:950px; height:300px', SYSDATE(), 0);
+INSERT INTO config (`id`, `token`, `value`, `date`, readonly) VALUES (NULL, 'chart_dimensions', 'width:950px; height:300px', SYSDATE(), 0);
 
-ALTER TABLE `config` DROP `description`;
+ALTER TABLE config DROP description;
+ALTER TABLE config ADD options VARCHAR(255) NOT NULL AFTER value;
+UPDATE config SET options="true,false" WHERE token="energy_meter_present"; 
+UPDATE config SET options="true,false" WHERE token="solar_meter_present"; 
+UPDATE config SET options="true,false" WHERE token="weather_meter_present"; 
+UPDATE config SET options="true,false" WHERE token="slide_show_on"; 
+UPDATE config SET options="right,none" WHERE token="chart_dimensions"; 
 
 
