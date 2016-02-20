1e46718103639f9ee14336b90f3ee52380b2733c
Willem van der Plaat <wplaat@gmail.com>
2016-02-20 09:12:21 +0100

    Add P1 analyse script to p1 sensor

--- src/sensors/p1/analyse.py
+++ src/sensors/p1/analyse.py
@@ -24,7 +24,6 @@ import datetime
 import locale
 import time
 import csv 
-import _mysql
 from time import strftime
 from sense_hat import SenseHat
 
@@ -44,42 +43,17 @@ ser.rtscts   = 0
 ser.timeout  = 20
 ser.port     = "/dev/ttyUSB0"
 
-lines = [line.rstrip('\n') for line in open('/var/www/html/plaatenergy/config.inc')]
-for line in lines:
-   if line[:1]=='$':
-     line = line.replace(' ','');
-     line = line.replace(';','');
-     line = line.replace('$','');
-     line = line.replace('"','');
-     key = line.split('=')
-     if key[0]=='dbhost':
-        dbhost=key[1]
-     if key[0]=='dbname':
-        dbname=key[1]
-     if key[0]=='dbuser':
-        dbuser=key[1]
-     if key[0]=='dbpass':
-        dbpass=key[1]
+print 'Start analyse';
+stack=[]
 
-con = _mysql.connect(dbhost, dbname, dbuser, dbpass)
+#Open COM port
+try:
+  ser.open()
+except:
+  sys.exit ("Fout bij het openen van poort %s. "  % ser.name)      
 
-sql = "select value from config where token='energy_meter_present'"
-con.query(sql)
-result = con.use_result()
-value = result.fetch_row()[0]
-con.close
-
-if value[0] == 'true':
-
-  stack=[]
-
-  #Open COM port
-  try:
-    ser.open()
-  except:
-    sys.exit ("Fout bij het openen van poort %s. "  % ser.name)      
-
-  while p1_log:
+line = 0 
+while p1_log:
     p1_line = ''
     try:
         p1_raw = ser.readline()
@@ -91,7 +65,8 @@ if value[0] == 'true':
     p1_str  = str(p1_raw)
     p1_line = p1_str.strip()
     stack.append(p1_line);
-    print (p1_line)
+    print (`line`+ '  ' + p1_line)
+    line = line + 1
 
     if p1_line[0:1] == "/":
         p1_telegram = True
@@ -103,13 +78,13 @@ if value[0] == 'true':
             p1_log      = False	
 
   #Close port and show status
-  try:
-    ser.close()
-  except:
-    sys.exit ("Fout bij het sluiten van %s. Programma afgebroken." % ser.name )      
+try:
+  ser.close()
+except:
+  sys.exit ("Fout bij het sluiten van %s. Programma afgebroken." % ser.name )      
 
-  stack_teller=0
-  while stack_teller < len(stack):
+stack_teller=0
+while stack_teller < len(stack):
    if stack[stack_teller][0:9] == "1-0:1.8.1":
       dal = float(stack[stack_teller][10:20])
    elif stack[stack_teller][0:9] == "1-0:1.8.2":
@@ -126,11 +101,17 @@ if value[0] == 'true':
       gas = float(stack[stack_teller][26:35])
    stack_teller = stack_teller +1
 
-  con = _mysql.connect(dbhost, dbname, dbuser, dbpass)
+print "==================";
+print "Dal = {0:.2f}".format(dal);
+print "Normal = {0:.2f}".format(piek);
+print "DalTerug = {0:.2f}".format(dalterug);
+print "NormalTerug = {0:.2f}".format(piekterug);
+print "Vermogen = {0:.2f}".format(vermogen);
+print "VermogenTerug = {0:.2f}".format(vermogenterug);
+print "Gas = {0:.2f}".format(gas);
+print "==================";
 
-  sql = "insert into energy( timestamp,dal,piek,dalterug,piekterug,vermogen,vermogenterug,gas) values (str_to_date('{0}','%d-%m-%Y %H:%i:%s'),{1},{2},{3},{4},{5},{6},{7})".format( strftime('%d-%m-%Y %H:%M:00', time.localtime()), dal, piek, dalterug, piekterug, vermogen, vermogenterug, gas)
-
-  con.query(sql)
+print 'End analyse';
 
 #
 # ---------------------
