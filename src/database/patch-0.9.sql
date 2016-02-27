fac83f3a2e3fa086d98950f97887c72c2eed0915
Willem van der Plaat <wplaat@gmail.com>
2016-02-27 15:06:18 +0100

    update sql

--- src/database/patch-0.9.sql
+++ src/database/patch-0.9.sql
@@ -16,7 +16,7 @@
 
 UPDATE config SET value="0.9" WHERE token='database_version';
 
-INSERT INTO config (`id`, `token`, `value`, `date`, readonly) VALUES (NULL, 'access_password', '', SYSDATE(), 0);
+INSERT INTO config (`id`, `token`, `value`, `date`, readonly) VALUES (NULL, 'home_password', '', SYSDATE(), 0);
 INSERT INTO config (`id`, `token`, `value`, `date`, readonly) VALUES (NULL, 'settings_password', '', SYSDATE(), 0);
 INSERT INTO config (`id`, `token`, `value`, `date`, readonly) VALUES (NULL, 'language', 'en', SYSDATE(), 1);
 INSERT INTO config (`id`, `token`, `value`, `date`, readonly) VALUES (NULL, 'theme', 'light', SYSDATE(), 1);
