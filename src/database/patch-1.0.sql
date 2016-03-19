5f8b7c406d95f5be27d534cf61088edee4d1981f
Willem van der Plaat <wplaat@gmail.com>
2016-03-19 20:08:01 +0100

    Improve upgrade script

--- src/database/patch-1.0.sql
+++ src/database/patch-1.0.sql
@@ -16,51 +16,22 @@
 
 UPDATE config SET value="1.0" WHERE token='database_version';
 
-CREATE TABLE IF NOT EXISTS `energy1` (
-  `id` int(11) NOT NULL,
-  `timestamp` datetime NOT NULL,
-  `low_used` double NOT NULL,
-  `normal_used` double NOT NULL,
-  `low_delivered` double NOT NULL,
-  `normal_delivered` double NOT NULL,
-  `power_used` double NOT NULL,
-  `power_delivered` double NOT NULL,
-  `gas_used` double NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+ALTER TABLE energy RENAME TO energy1; 
+ALTER TABLE energy1 CHANGE `dal` `low_used` DOUBLE NOT NULL;
+ALTER TABLE energy1 CHANGE `piek` `normal_used` DOUBLE NOT NULL;
+ALTER TABLE energy1 CHANGE `dalterug` `low_delivered` DOUBLE NOT NULL;
+ALTER TABLE energy1 CHANGE `piekterug` `normal_delivered` DOUBLE NOT NULL;
+ALTER TABLE energy1 CHANGE `vermogen` `power` DOUBLE NOT NULL;
+ALTER TABLE energy1 CHANGE `gas` `gas_used` DOUBLE NOT NULL;
+UPDATE energy1 SET power = (vermogenterug*-1) where vermogenterug>0;
+ALTER TABLE energy1 DROP vermogenterug;
 
+ALTER TABLE energy_day RENAME TO energy_summary; 
+ALTER TABLE energy_summary CHANGE `dal` `low_used` DOUBLE NOT NULL;
+ALTER TABLE energy_summary CHANGE `piek` `normal_used` DOUBLE NOT NULL;
+ALTER TABLE energy_summary CHANGE `dalterug` `low_delivered` DOUBLE NOT NULL;
+ALTER TABLE energy_summary CHANGE `piekterug` `normal_delivered` DOUBLE NOT NULL;
+ALTER TABLE energy_summary CHANGE `vermogen` `power` DOUBLE NOT NULL;
+ALTER TABLE energy_summary CHANGE `solar` `solar_delivered` DOUBLE NOT NULL;
+ALTER TABLE energy_summary CHANGE `gas` `gas_used` DOUBLE NOT NULL;
 
-ALTER TABLE `energy1`
- ADD PRIMARY KEY (`id`);
- 
-ALTER TABLE `energy1`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
- 
-ALTER TABLE `energy1` ADD UNIQUE(`id`);
- 
--- Migrate data to new structure
-INSERT INTO energy1 (timestamp, low_used, normal_used, low_delivered, normal_delivered, power_used, power_delivered, gas_used)
-SELECT timestamp, dal, piek, dalterug, piekterug, vermogen, vermogenterug, gas FROM energy;
-UPDATE energy1 SET power_used = (power_delivered*-1) where power_delivered>0;
-ALTER TABLE energy1 DROP power_delivered;
-ALTER TABLE energy1 CHANGE power_used power DOUBLE NOT NULL;
-
-CREATE TABLE IF NOT EXISTS `energy_summary` (
-  `id` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `low_used` double NOT NULL,
-  `normal_used` double NOT NULL,
-  `low_delivered` double NOT NULL,
-  `normal_delivered` double NOT NULL,
-  `solar_delivered` double NOT NULL,
-  `gas_used` double NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-
-ALTER TABLE `energy_summary`
- ADD PRIMARY KEY (`id`);
- 
-ALTER TABLE `energy_summary`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
-
--- Migrate data to new structure
-INSERT INTO energy_summary (date, low_used, normal_used, low_delivered, normal_delivered, solar_delivered, gas_used)
-SELECT date, dal, piek, dalterug, piekterug, solar, gas FROM energy_day;
