04e0ff7cf6f87a45bbb6455468e30d4f36e40f99
Willem van der Plaat <wplaat@gmail.com>
2016-03-29 21:02:32 +0200

    Update release notes

--- src/release_notes.php
+++ src/release_notes.php
@@ -27,15 +27,15 @@
 ** ---------------------
 */
 
-$note[10] = '<div class="subparagraph">Version 1.0 (28-03-2016)</div>
+$note[10] = '<div class="subparagraph">Version 1.0 (29-03-2016)</div>
 <div class="large_text">
 <ul>
 <li>General: Add support for maximum 3 solar converter meters.</li>
 <li>General: Improve settings page - added setting categories.</li>
-<li>General: Add support for Hosola Bright Solar converter meter support.</li>
+<li>General: Add support for Hosola Bright Solar converter meter.</li>
 <li>General: Refactor database model - All entity names are now english.</li>
-<li>General: Rebuild database only when needed if configuration change.</li>
-<li>General: Add configuration option to disable gas meter.</li>
+<li>General: Add setting option to set initial solar meter reading.</li>
+<li>General: Add setting option to disable gas meter.</li>
 </ul>
 </div>';
 
