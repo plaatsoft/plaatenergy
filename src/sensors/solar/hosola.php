1e064a1a9c25cdb7e7418779a5eda719df3526c2
Willem van der Plaat <wplaat@gmail.com>
2016-04-18 21:47:12 +0200

    Update solar sensors php scripts

--- src/sensors/solar/hosola.php
+++ src/sensors/solar/hosola.php
@@ -32,37 +32,40 @@ $port = plaatenergy_db_get_config_item('solar_meter_port', $instance);
 $sn = plaatenergy_db_get_config_item('solar_meter_serial_number', $instance);
 
 $inverter = new Inverter($ip,$port,$sn);				
-if ($inverter->read()===false) {
+if ($inverter->read()==false) {
+
 	echo "$inverter->errorcode : $inverter->error";			
-}
-		
-$data = json_decode($inverter->power());
-
-$timestamp = date('Y-m-d H:i:00');
-
-if (isset($data->pac1) && ($data->pac1>0)) 
-{
-	$sql1 = 'select id from solar'.$index.' where timestamp="'.$timestamp.'"';
-	$result1 = plaatenergy_db_query($sql1);
-	$data1 = plaatenergy_db_fetch_object($result1);
 	
-	if ( isset($data1->id) ) {
-
-		$sql  = 'update solar'.$index.' set temp='.$data->temperature.', vdc1='.$data->vdc1.', ';
-		$sql .= 'vdc2='.$data->vdc2.', idc1='.$data->idc1.', ';
-		$sql .= 'idc2='.$data->idc2.', iac='.$data->iac1.', ';
-		$sql .= 'vac='.$data->vac2.', fac='.$data->fac1.', ';
-		$sql .= 'pac='.$data->pac1.', etoday='.$data->etoday.',';
-		$sql .= 'etotal='.$data->etotal.' where id='.$data1->id;
+} else {
 		
-	} else {
+	$data = json_decode($inverter->power());
+
+	$timestamp = date('Y-m-d H:i:00');
 	
-		$sql  = 'insert into solar'.$index.' ( timestamp, temp,vdc1,vdc2,idc1,idc2,iac,vac,fac,pac,etoday,etotal) ';
-		$sql .= 'values ("'.$timestamp.'",'.$data->temperature.','.$data->vdc1.','.$data->vdc2.','.$data->idc1.','.
-		$data->idc2.','.$data->iac1.','.$data->vac1.','.$data->fac1.','.$data->pac1.','.$data->etoday.','.$data->etotal.')';
+	if (isset($data->pac1) && ($data->pac1>0)) 
+	{
+		$sql1 = 'select id from solar'.$index.' where timestamp="'.$timestamp.'"';
+		$result1 = plaatenergy_db_query($sql1);
+		$data1 = plaatenergy_db_fetch_object($result1);
+		
+		if ( isset($data1->id) ) {
+	
+			$sql  = 'update solar'.$index.' set temp='.$data->temperature.', vdc1='.$data->vdc1.', ';
+			$sql .= 'vdc2='.$data->vdc2.', idc1='.$data->idc1.', ';
+			$sql .= 'idc2='.$data->idc2.', iac='.$data->iac1.', ';
+			$sql .= 'vac='.$data->vac2.', fac='.$data->fac1.', ';
+			$sql .= 'pac='.$data->pac1.', etoday='.$data->etoday.', ';
+			$sql .= 'etotal='.$data->etotal.' where id='.$data1->id;
+			
+		} else {
+			
+			$sql  = 'insert into solar'.$index.' ( timestamp, temp,vdc1,vdc2,idc1,idc2,iac,vac,fac,pac,etoday,etotal) ';
+			$sql .= 'values ("'.$timestamp.'",'.$data->temperature.','.$data->vdc1.','.$data->vdc2.','.$data->idc1.','.
+			$data->idc2.','.$data->iac1.','.$data->vac1.','.$data->fac1.','.$data->pac1.','.$data->etoday.','.$data->etotal.')';
+		}
+			
+		plaatenergy_db_query($sql);
 	}
-		
-	plaatenergy_db_query($sql);
 }
 
 ?>
--- src/sensors/solar/omnik.php
+++ src/sensors/solar/omnik.php
@@ -30,38 +30,42 @@ $ip = plaatenergy_db_get_config_item('solar_meter_ip', $instance);
 $port = plaatenergy_db_get_config_item('solar_meter_port', $instance);
 $sn = plaatenergy_db_get_config_item('solar_meter_serial_number', $instance);
 
-$inverter	=	new Inverter($ip,$port,$sn);				
-if ($inverter->read()===false) {
-	echo "$inverter->errorcode : $inverter->error";			
-}
-		
-$data = json_decode($inverter->power());
+$inverter = new Inverter($ip,$port,$sn);				
 
-$timestamp = date('Y-m-d H:i:00');
+if ($inverter->read()==false) {
 
-if (isset($data->pac1) && ($data->pac1>0)) 
-{
-	$sql1 = 'select id from solar'.$index.' where timestamp="'.$timestamp.'"';
-	$result1 = plaatenergy_db_query($sql1);
-	$data1 = plaatenergy_db_fetch_object($result1);
+	echo "$inverter->errorcode : $inverter->error";		
 	
-	if ( isset($data1->id) ) {
-
-		$sql  = 'update solar'.$index.' set temp='.$data->temperature.', vdc1='.$data->vdc1.', ';
-		$sql .= 'vdc2='.$data->vdc2.', idc1='.$data->idc1.', ';
-		$sql .= 'idc2='.$data->idc2.', iac='.$data->iac1.', ';
-		$sql .= 'vac='.$data->vac2.', fac='.$data->fac1.', ';
-		$sql .= 'pac='.$data->pac1.', etoday='.$data->etoday.',';
-		$sql .= 'etotal='.$data->etotal.' where id='.$data1->id;
+} else {
 		
-	} else {
+	$data = json_decode($inverter->power());
+
+	$timestamp = date('Y-m-d H:i:00');
+
+	if (isset($data->pac1) && ($data->pac1>0)) 
+	{
+		$sql1 = 'select id from solar'.$index.' where timestamp="'.$timestamp.'"';
+		$result1 = plaatenergy_db_query($sql1);
+		$data1 = plaatenergy_db_fetch_object($result1);
+		
+		if ( isset($data1->id) ) {
 	
-		$sql  = 'insert into solar'.$index.' ( timestamp, temp,vdc1,vdc2,idc1,idc2,iac,vac,fac,pac,etoday,etotal) ';
-		$sql .= 'values ("'.$timestamp.'",'.$data->temperature.','.$data->vdc1.','.$data->vdc2.','.$data->idc1.','.
-		$data->idc2.','.$data->iac1.','.$data->vac1.','.$data->fac1.','.$data->pac1.','.$data->etoday.','.$data->etotal.')';
+			$sql  = 'update solar'.$index.' set temp='.$data->temperature.', vdc1='.$data->vdc1.', ';
+			$sql .= 'vdc2='.$data->vdc2.', idc1='.$data->idc1.', ';
+			$sql .= 'idc2='.$data->idc2.', iac='.$data->iac1.', ';
+			$sql .= 'vac='.$data->vac2.', fac='.$data->fac1.', ';
+			$sql .= 'pac='.$data->pac1.', etoday='.$data->etoday.', ';
+			$sql .= 'etotal='.$data->etotal.' where id='.$data1->id;
+			
+		} else {
+		
+			$sql  = 'insert into solar'.$index.' ( timestamp, temp,vdc1,vdc2,idc1,idc2,iac,vac,fac,pac,etoday,etotal) ';
+			$sql .= 'values ("'.$timestamp.'",'.$data->temperature.','.$data->vdc1.','.$data->vdc2.','.$data->idc1.','.
+			$data->idc2.','.$data->iac1.','.$data->vac1.','.$data->fac1.','.$data->pac1.','.$data->etoday.','.$data->etotal.')';
+		}
+			
+		plaatenergy_db_query($sql);
 	}
-		
-	plaatenergy_db_query($sql);
 }
 	
 ?>
