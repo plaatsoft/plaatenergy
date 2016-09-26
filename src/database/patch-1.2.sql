5fc15975265fd126bf8880274e8dc0b7a6751b39
Willem van der Plaat <wplaat@gmail.com>
2016-09-26 20:14:04 +0200

    Found two major bugs during install from scratch. This is fix now!

--- src/database/patch-1.2.sql
+++ src/database/patch-1.2.sql
@@ -17,9 +17,6 @@
 
 UPDATE config SET value="1.2" WHERE token='database_version';
 
-INSERT INTO config (category, token, value, options, readonly, rebuild) VALUES (81, 'notification_present', 'false', 'true,false', 0, 0);
-INSERT INTO config (category, token, value, options, readonly, rebuild) VALUES (81, 'notification_nma_key', '', '', 0, 0);
-
 ALTER TABLE config ADD encrypt INT NOT NULL AFTER `rebuild`;
 UPDATE config SET encrypt = 1, value="" WHERE token = "home_password";
 UPDATE config SET encrypt = 1, value="" WHERE token = "settings_password";
